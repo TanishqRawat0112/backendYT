ff92d5ff6351d5b9bb113083b579a62af5a8b2dc
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-09-09 01:31:38 +0530

    util code discussion

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,11 +1,21 @@
-// const asyncHandler = () => {
-
-// }
-
-
+const asyncHandler = (requestHandler) => {
+    (res,req,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((error) => {
+            next(error);
+        }
+    )
+    }
+}
 
 export {asyncHandler};
 
-const asyncHandler = () =>{
-    
-}
+// const asyncHandler = (fn) => async(req,res,next) => {
+//     try {
+//         await fn(req,res,next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message || "Internal Server Error"
+//         })
+//     }
+// }

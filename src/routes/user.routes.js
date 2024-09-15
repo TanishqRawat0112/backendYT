75198560db89dff61442496cac0d8eff96a127be
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-09-15 16:56:52 +0530

    refactor: Update user.routes.js to include new endpoints and modify existing ones

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import {Router} from "express";
-import { registerUser,loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateUserDetails,updateAvatar,updateCoverImage } from "../controllers/user.controller.js";
+import { registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateUserDetails,
+    updateAvatar,
+    updateCoverImage,
+    getUserChannelProfile,
+    getWatchHistory } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -28,9 +38,13 @@ router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword );
 router.route("/current-user").get(verifyJWT, getCurrentUser );
-router.route("/update-details").post(verifyJWT,updateUserDetails );
-router.route("/update-details/update-avatar").post(verifyJWT, upload.single("avatar"), updateAvatar );
-router.route("/update-details/update-cover-image").post(verifyJWT, upload.single("coverImage"), updateCoverImage );
+router.route("/update-details").patch(verifyJWT,updateUserDetails );
+router.route("/update-details/update-avatar").patch(verifyJWT, upload.single("avatar"), updateAvatar );
+router.route("/update-details/update-cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage );
+
+router.route("/c/:username").get(getUserChannelProfile);
+
+router.route("history").get(verifyJWT,getWatchHistory);
 
 export default router;
 

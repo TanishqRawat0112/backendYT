0f0347e04f4c7d8917b6d93a4f872222b3d798b1
TanishqRawat0112 <21U02036@iiitbhopal.ac.in>
2024-09-20 00:04:27 +0530

    refactor: Update user.routes.js to include new endpoints and modify existing ones

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -36,7 +36,7 @@ router.route("/login").post(upload.none(),loginUser);
 
 //secured route
 
-router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/logout").post(verifyJWT,upload.none(), logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT,upload.none(), changeCurrentPassword );
 router.route("/current-user").get(verifyJWT, getCurrentUser );
